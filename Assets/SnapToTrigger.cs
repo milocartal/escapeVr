d5d39145da08c15a7bfd4136e5a824fd4a51bedf
Reumiii <Remi.CHABRERIE@etu.uca.fr>
2023-11-24 10:55:22 +0100

    Update snap
    Disable interaction when snapping object to trigger

--- Assets/SnapToTrigger.cs
+++ Assets/SnapToTrigger.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class SnapToTrigger : MonoBehaviour
 {
+    private XRGrabInteractable interactable;
+
+    private void Start()
+    {
+        interactable = GetComponent<XRGrabInteractable>();
+    }
+
     public void Snap(Vector3 trigger_pos)
     {
+        interactable.enabled = false;
         gameObject.transform.position = trigger_pos;    
     }
 }
